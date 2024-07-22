5a6ec59dc715e5e418388f7911883b030214b51f
Himanshi Mate <matemanu725@gmail.com>
2024-07-22 18:36:36 +0530

    remark

--- project/app/views.py
+++ project/app/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from .models import Student
 # Create your views here.
+def home(request):
+    data=Student.objects.all()                   #================show all the data
+    print(data)
+    data=Student.objects.all().first()                       # 2type of query ---> single object query,multiple object query
+    print(data)
+    data=Student.objects.all().filter(stu_city="bhopal")     #==============filter
+    print(data.values())
+
+    data=Student.objects.all().exclude(stu_name="neeraj")     #============which object is excluded
+    print(data.values())
+
+    data=Student.objects.all().order_by('stu_name')  # =========object arrange in ascending order=======
+    print(data.values())
+    data=Student.objects.values('stu_name')   #==============show values
+    print(data)
+
+    data=Student.objects.values('stu_name')[::2]   #==========slicing
+    print(data)
+    
+
+
+
+    return HttpResponse("hello..............................")
