2c83c8ed3d5dec74977f625071b57ed3c96ac0a9
Himanshi Mate <matemanu725@gmail.com>
2024-07-23 18:30:25 +0530

    remark

--- project/app/views.py
+++ project/app/views.py
@@ -3,25 +3,49 @@ from django.http import HttpResponse
 from .models import Student
 # Create your views here.
 def home(request):
-    data=Student.objects.all()                   #================show all the data
-    print(data)
-    data=Student.objects.all().first()                       # 2type of query ---> single object query,multiple object query
-    print(data)
-    data=Student.objects.all().filter(stu_city="bhopal")     #==============filter
-    print(data.values())
+    # data=Student.objects.all()                   #================show all the data
+    # print(data)
+    # data=Student.objects.all().first()                       # 2type of query ---> single object query,multiple object query
+    # print(data)
+    # data=Student.objects.all().filter(stu_city="bhopal")     #==============filter
+    # print(data.values())
 
-    data=Student.objects.all().exclude(stu_name="neeraj")     #============which object is excluded
-    print(data.values())
+    # data=Student.objects.all().exclude(stu_name="neeraj")     #============which object is excluded
+    # print(data.values())
 
-    data=Student.objects.all().order_by('stu_name')  # =========object arrange in ascending order=======
-    print(data.values())
-    data=Student.objects.values('stu_name')   #==============show values
-    print(data)
+    # data=Student.objects.all().order_by('stu_name')  # =========object arrange in ascending order=======
+    # print(data.values())
+    # data=Student.objects.values('stu_name')   #==============show values
+    # print(data)
 
-    data=Student.objects.values('stu_name')[::2]   #==========slicing
-    print(data)
+    # data=Student.objects.values('stu_name')[::2]   #==========slicing
+    # print(data)
+ 
+
+#=================================single object query=======================================,,,,.
+    # data0 = Student.objects.get(id=2)
+    # print(data0)
+
+    # data0 = Student.objects.first()
+    # print(data0)
     
+#  ===============order the data in ascending and descending order============================
+    # data0 = Student.objects.order_by("stu_name").first()
+    # print(data0)
 
+    # data0 = Student.objects.order_by("-stu_name").first()
+    # print(data0)
 
+    # ==================create the data in database=================
+    '''
+    data0=Student.objects.create(stu_name='sakshi',stu_city='mumbai')
+    print(data0.stu_name,data0.stu_city)
+    '''
 
-    return HttpResponse("hello..............................")
+    # =================get_or_create=======combine=============
+    '''
+    data,created=Student.objects.get_or_create(stu_name="raj",stu_city="jabalpur",stu_email="raj@gmail.com")
+    print(data.id,data.stu_name,data.stu_city,data.stu_email)
+    '''
+
+    return HttpResponse('hello...................')
